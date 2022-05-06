e2f3108482644e12c71a61973cfe713e42b6394d
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-06 09:38:36 -0400

    Creados componentes para usar, definido layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BarComponent } from './bar/bar.component';
+import { FooComponent } from './foo/foo.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FooComponent },
+  { path: 'foo', component: FooComponent },
+  { path: 'bar', component: BarComponent },
+  { path: '**', component: NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { FooComponent } from './foo/foo.component';
+import { BarComponent } from './bar/bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    FooComponent,
+    BarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
